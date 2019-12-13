7a4355a322cd4c295d5c5865800f5b71c37dacda
Bielenin <Tomasz.Bielenin@jacobs.com>
2019-12-13 15:51:03 +0100

    Set name added

--- clashtestbuilder.py
+++ clashtestbuilder.py
@@ -6,7 +6,7 @@ src = "C:/Scripting/Git/clashTestBuilder/CNEB_SearchSets.xml"
 dst = "C:/Scripting/Git/clashTestBuilder/output.xml"
 
 import xml.etree.ElementTree as ET
-import copy
+# import copy
 
 sroot = ET.parse(src).getroot()
 
@@ -32,7 +32,11 @@ tmp = """<?xml version="1.0" encoding="UTF-8" ?>
 <exchange xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd" units="m" filename="" filepath="">
   <batchtest name="CNEB_ClashTests" internal_name="CNEB_ClashTests" units="m">
     <clashtests>
-      <clashtest name="Carriageway vs Carriageway" test_type="hard" status="new" tolerance="0.0000000000" merge_composites="0">
+    </clashtests>
+  </batchtest>
+</exchange>"""
+
+ttest = """<clashtest name="Carriageway vs Carriageway" test_type="hard" status="new" tolerance="0.0000000000" merge_composites="0">
         <linkage mode="none"/>
         <left>
           <clashselection selfintersect="0" primtypes="1">
@@ -45,10 +49,7 @@ tmp = """<?xml version="1.0" encoding="UTF-8" ?>
           </clashselection>
         </right>
         <rules/>
-      </clashtest>
-    </clashtests>
-  </batchtest>
-</exchange>"""
+      </clashtest>"""
 
 # droot = ET.parse(dst).getroot()
 # print((droot.findall('batchtest/clashtests/clashtest')[2].get('name')))
@@ -57,16 +58,28 @@ tmp = """<?xml version="1.0" encoding="UTF-8" ?>
 
 # while dlen < len(ctests):
 #     droot[0][0].append(copy.deepcopy(droot[0][0][0]))
-troot = ET.fromstring(tmp)
-b = troot.find("batchtest/clashtests")
-c = troot.find("batchtest/clashtests/clashtest")
+droot = ET.fromstring(tmp)
+a = droot.find("batchtest")
+b = droot.find("batchtest/clashtests")
+# c = ET.fromstring(ttest)
+d = sroot.find("selectionsets")
 # dupe = copy.deepcopy(c) #copy <c> node
-b.append(c) #insert the new node
+a.append(d)
+nclist = []
+for i in range(3):
+  c = ET.fromstring(ttest)
+  nname = " ".join((ctests[i][0], "vs", ctests[i][1]))
+  print(nname)
+  c.set('name', nname)
+  nclist.append(ET.tostring(c))
+  b.append(ET.fromstring(nclist[i]))
 
-tree = ET.ElementTree(troot)
-dst = open("C:/Scripting/Git/clashTestBuilder/output.xml", "w")
-tree.write((dst),encoding='unicode')
-dst.close()
+
+
+tree = ET.ElementTree(droot)
+f = open(dst, "w")
+tree.write((f),encoding='unicode')
+f.close()
 # - scrap search set names
 # - file name from parameter
 # - create test sets
